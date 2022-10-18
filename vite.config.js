77f30905c245284a5b5367068c9fe39eb0b0ea61
EMarines <emarines@live.com.mx>
2022-10-17 18:59:44 -0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/subirPage/"    ( cambié de este al de abajo para quiter el /subirPage/)
-  base: "/",
+  base: "/subirPage/",
   plugins: [svelte()]
 
 })
